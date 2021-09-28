b1dd0b1b015b10588342fd49d1e874aa0cf7627d
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-28 15:57:11 -0400

    Add project files.

---
+++ BankingProject/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject
+{
+    class Account
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; } = 0;
+        public string Description { get; set; }
+        
+        public bool Deposit(decimal Amount)
+        {
+            this.Balance += Amount;
+            return true;
+        }
+
+        public bool Withdraw(decimal Amount)
+        {
+            if(Amount > this.Balance)
+            {
+                Console.WriteLine("Insufficient Funds");
+                return false;
+            }
+            this.Balance += -Amount;
+            return true;
+        }
+        public bool Transfer(decimal Amount, Account ToAccount)
+        {
+            var success = Withdraw(Amount);
+            if (success)
+            {
+                ToAccount.Deposit(Amount);
+            }
+            return true;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{Id} | {Balance} | {Description}");
+        }
+    }
+}
---
+++ BankingProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BankingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acct1 = new Account()
+            {
+                Id = 1,
+                Description = "Test Account"
+            };
+            acct1.Print();
+            acct1.Deposit(135);
+            acct1.Print();
+            acct1.Withdraw(270);
+            acct1.Print();
+            var acct2 = new Account()
+            {
+                Id = 2,
+                Description = "New Account"
+            };
+            acct1.Transfer(25, acct2);
+            acct1.Print();
+            acct2.Print();
+
+            acct1.Withdraw(-1000);
+            acct1.Print();
+        }
+    }
+}
